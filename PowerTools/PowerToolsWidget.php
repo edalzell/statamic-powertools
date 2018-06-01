31dab9f5add2277d66a47de47975d2f9b6242d9d
edalzell <Erin@thedalzells.org>
2018-05-31 19:59:01 -0700

    Don’t need to send the setting

--- PowerTools/PowerToolsWidget.php
+++ PowerTools/PowerToolsWidget.php
@@ -6,7 +6,6 @@ use Statamic\Extend\Widget;
 
 class PowerToolsWidget extends Widget
 {
-
     /**
      * The HTML that should be shown in the widget
      *
@@ -15,9 +14,8 @@ class PowerToolsWidget extends Widget
     public function html()
     {
         // Get settings
-        $settings    = $this->getConfig();
-        $hide_presets = $this->getConfig('hide_presets');
+        $settings = $this->getConfig();
         $github_page = $this->getMeta()['url'];
-        return $this->view('widget', compact('settings', 'hide_presets', 'github_page'))->render();
+        return $this->view('widget', compact('settings', 'github_page'))->render();
     }
 }
