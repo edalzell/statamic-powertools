ff1f54b3cca596f6bcfc087af0c199eafd5878ed
edalzell <Erin@thedalzells.org>
2018-05-31 19:58:49 -0700

    Updated formatting and removed setting

--- PowerTools/resources/views/widget.blade.php
+++ PowerTools/resources/views/widget.blade.php
@@ -19,29 +19,29 @@
     <div class="card-body pad-16">
       <div class="form-group text-fieldtype width-100">
         <div class="field-inner">
-			       <label class="block">Search</label>
-             <a href="{{ route('powertools.rebuild-search') }}" class="btn">Update Search Index</a>
+			<label class="block">Search</label>
+            <a href="{{ route('powertools.rebuild-search') }}" class="btn">Update Search Index</a>
         </div>
       </div>
       <div class="form-group text-fieldtype width-100">
         <div class="field-inner">
-			       <label class="block">Caching</label>
-             <div class="btn-group">
-                 <a href="{{ route('powertools.clear-cache') }}" class="btn">Clear Cache</a>
-                 <a href="{{ route('powertools.update-stache') }}" class="btn">Clear Stache</a>
-                 <a href="{{ route('powertools.clear-static') }}" class="btn">Clear Static</a>
-                 <a href="{{ route('powertools.clear-glide') }}" class="btn">Clear Glide</a>
-             </div>
+			<label class="block">Caching</label>
+            <div class="btn-group">
+                <a href="{{ route('powertools.clear-cache') }}" class="btn">Clear Cache</a>
+                <a href="{{ route('powertools.update-stache') }}" class="btn">Clear Stache</a>
+                <a href="{{ route('powertools.clear-static') }}" class="btn">Clear Static</a>
+                <a href="{{ route('powertools.clear-glide') }}" class="btn">Clear Glide</a>
+            </div>
         </div>
       </div>
-      @unless ($hide_presets)
+      @unless ($settings['hide_presets'])
       <div class="form-group text-fieldtype width-100">
         <div class="field-inner">
-			       <label class="block">Image Manipulation Presets</label>
-             <div class="btn-group">
-               <a href="{{ route('powertools.assets-generate-presets') }}" class="btn">Generate Assets</a>
-               <a href="{{ route('powertools.assets-regenerate-presets') }}" class="btn">Regenerate Assets</a>
-             </div>
+			<label class="block">Image Manipulation Presets</label>
+            <div class="btn-group">
+                <a href="{{ route('powertools.assets-generate-presets') }}" class="btn">Generate Assets</a>
+                <a href="{{ route('powertools.assets-regenerate-presets') }}" class="btn">Regenerate Assets</a>
+            </div>
         </div>
       </div>
       @endunless
